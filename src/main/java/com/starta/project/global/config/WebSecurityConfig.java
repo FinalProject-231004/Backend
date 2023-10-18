63e35272d3bfe45f1970641c6f870a42a3a69e7b
heypop <hey@herenas.com>
2023-10-18 23:51:21 +0900

    Refactor: 시큐리티 config 수정 - quiz controller 접근권한 재설정 - 타임리프 종속성 주석처리 - 폼로그인 비활성화

--- src/main/java/com/starta/project/global/config/WebSecurityConfig.java
+++ src/main/java/com/starta/project/global/config/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -91,15 +92,13 @@ public class WebSecurityConfig implements WebMvcConfigurer {
                                 .antMatchers("/api/member/**").permitAll()
                                 .antMatchers("/v3/api-docs/**").permitAll()
                                 .antMatchers("/swagger-ui/**").permitAll()
+                                .antMatchers("/api/quiz/**").permitAll()
                                 .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
         http.cors();
 
-        http.formLogin((formLogin) ->
-                formLogin
-                        .loginPage("/api/member/login-page").permitAll()
-        );
+        http.formLogin(AbstractHttpConfigurer::disable); // 폼 로그인 비활성화
 
         // 필터 관리
         http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
