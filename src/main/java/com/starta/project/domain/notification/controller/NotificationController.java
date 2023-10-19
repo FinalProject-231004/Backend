ab205e636ced9de71aaec8b3c7f1955dc73b5fc2
SangYoungBaek <bsy2274@naver.com>
2023-10-19 15:59:53 +0900

    Refactor:notification, sse 컨트롤러 UserDetailsImpl 히든처리

--- src/main/java/com/starta/project/domain/notification/controller/NotificationController.java
+++ src/main/java/com/starta/project/domain/notification/controller/NotificationController.java
@@ -3,6 +3,8 @@ package com.starta.project.domain.notification.controller;
 import com.starta.project.domain.notification.entity.Notification;
 import com.starta.project.domain.notification.service.NotificationService;
 import com.starta.project.global.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -19,17 +21,20 @@ public class NotificationController {
 
     private final NotificationService notificationService;
 
+    @Operation(summary = "알림전체조회")
     @GetMapping("/notification")
-    public ResponseEntity<List<Notification>> getAllNotificationByUsername(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<List<Notification>> getAllNotificationByUsername(@Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return ResponseEntity.ok().body(notificationService.getAllNotificationByUsername(userDetails.getUsername()));
     }
 
+    @Operation(summary = "알림전체읽음")
     @PutMapping("/notification/{username}/read")
     public ResponseEntity<List<Notification>> updateNotificationReadStatusByUsername(@PathVariable String username) {
         notificationService.updateNotificationReadStatusByUsername(username);
         return ResponseEntity.ok().body(notificationService.getAllNotificationByUsername(username)); //수정 후 새롭게 전달
     }
 
+    @Operation(summary = "알림삭제")
     @PutMapping("/notification/{id}/delete")
     public ResponseEntity<List<Notification>> updateNotificationDeleteStatusById(@PathVariable("id") String notificationId) {
         notificationService.updateNotificationDeleteStatusById(notificationId);
