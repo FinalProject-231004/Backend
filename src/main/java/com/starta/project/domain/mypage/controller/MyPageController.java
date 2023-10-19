308c0ca13e1c54a27b74f117fd7521ff6f760a5a
heypop <hey@herenas.com>
2023-10-19 15:54:26 +0900

    Refactor: 스웨거 관련 컨트롤러 UserDetailsImpl 히든처리

--- src/main/java/com/starta/project/domain/mypage/controller/MyPageController.java
+++ src/main/java/com/starta/project/domain/mypage/controller/MyPageController.java
@@ -1,17 +1,16 @@
 package com.starta.project.domain.mypage.controller;
 
-import com.starta.project.domain.mileageshop.entity.ItemCategoryEnum;
 import com.starta.project.domain.mypage.service.MyPageService;
 import com.starta.project.domain.quiz.entity.Quiz;
 import com.starta.project.global.messageDto.MsgDataResponse;
 import com.starta.project.global.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,13 +25,13 @@ public class MyPageController {
 
     @Operation(summary = "마일리지 구매내역")
     @GetMapping("/mypage/purchase-history")
-    public ResponseEntity<MsgDataResponse> getItemsByCategory(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<MsgDataResponse> getItemsByCategory(@Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return ResponseEntity.status(HttpStatus.OK).body(myPageService.getPurchaseHistory(userDetails.getMember()));
     }
 
     @Operation(summary = "임시 게시글 조회")
     @GetMapping("/mypage/un-display")
-    public ResponseEntity<List<Quiz>> showUnDisplayQuiz(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<List<Quiz>> showUnDisplayQuiz(@Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return ResponseEntity.ok(myPageService.showUnDisplayQuiz(userDetails.getMember()));
     }
 }
