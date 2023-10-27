08c90f8cdcacbff1aa7f175d466f31a874699a37
heypop <hey@herenas.com>
2023-10-28 07:58:15 +0900

    Feat: 댓글만 따로 조회 가능하도록 댓글 생성 API 추가 - 그 외 댓글 CUD 부분 반환값 변경

--- src/main/java/com/starta/project/domain/quiz/dto/CommentCreateRequestDto.java
+++ src/main/java/com/starta/project/domain/quiz/dto/CommentCreateRequestDto.java
@@ -3,6 +3,7 @@ package com.starta.project.domain.quiz.dto;
 import lombok.Getter;
 
 @Getter
-public class CreateCommentRequestDto {
+public class CommentCreateRequestDto {
+    private Long quizId;
     private String content;
 }
