628bc22f6a1f94268be2a5cc8c27a0f9b3ad2f88
heypop <hey@herenas.com>
2023-11-07 20:11:25 +0900

    Refactor: 기존 Controller에서 로직 수행하던 것 Service로 파일 분리

--- src/main/java/com/starta/project/domain/liveQuiz/controller/LiveQuizController.java
+++ src/main/java/com/starta/project/domain/liveQuiz/controller/LiveQuizController.java
@@ -1,10 +1,13 @@
 package com.starta.project.domain.liveQuiz.controller;
 
-import com.starta.project.domain.liveQuiz.dto.ChatMessage;
+import com.starta.project.domain.liveQuiz.dto.ChatMessageDto;
 import com.starta.project.domain.liveQuiz.service.LiveQuizService;
+import com.starta.project.global.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
@@ -15,18 +18,17 @@ import java.time.format.DateTimeFormatter;
 @RequiredArgsConstructor
 public class LiveQuizController {
 
-    LiveQuizService liveQuizService;
+    private final LiveQuizService liveQuizService;
 
     // 클라이언트가 /app/sendMassage로 메시지를 보내면, 서버는 /topic/liveQuizChatRoom로 메시지를 보낸다.
     @MessageMapping("/liveQuizSendMassage")
-    @SendTo("/topic/liveQuizChatRoom")
-    public ChatMessage sendMessage(ChatMessage chatMessage) {
-        if (chatMessage != null && chatMessage.getMessage() != null) {
-            String escapedMessage = HtmlUtils.htmlEscape(chatMessage.getMessage());
-            chatMessage.setMessage(escapedMessage);
-            chatMessage.setTimestamp(getCurrentTime());
-        }
-        return chatMessage;
+    @SendTo("/api/liveQuizChatRoom")
+    public ChatMessageDto sendMessage(
+//            @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails,
+            ChatMessageDto chatMessage) {
+
+        return liveQuizService.sendMessage(chatMessage);
+
     }
 
     // 현재 시간을 반환하는 메소드
--- src/main/java/com/starta/project/domain/liveQuiz/service/LiveQuizService.java
+++ src/main/java/com/starta/project/domain/liveQuiz/service/LiveQuizService.java
@@ -1,15 +1,23 @@
 package com.starta.project.domain.liveQuiz.service;
 
+
+import com.starta.project.domain.liveQuiz.dto.ChatMessageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Service
 @RequiredArgsConstructor
 public class LiveQuizService {
 
 
-
+    public ChatMessageDto sendMessage(ChatMessageDto chatMessage) {
+        if (chatMessage != null && chatMessage.getMessage() != null) {
+            String escapedMessage = HtmlUtils.htmlEscape(chatMessage.getMessage());
+            chatMessage = new ChatMessageDto("꺄뀻", escapedMessage, LocalDateTime.now());
+        }
+        return chatMessage;
+    }
 }
