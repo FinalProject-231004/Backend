c0148545cd1cdf6691250d94241d4dd36c360f62
heypop <hey@herenas.com>
2023-11-07 20:11:01 +0900

    Refactor: username 필드 생성

---
+++ src/main/java/com/starta/project/domain/liveQuiz/dto/ChatMessageDto.java
@@ -0,0 +1,22 @@
+package com.starta.project.domain.liveQuiz.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@NoArgsConstructor
+public class ChatMessageDto {
+    private String username; // 메시지 보낸 사람
+    private String message; // 메시지 내용
+    private String timestamp; // 메시지 보낸 시간
+
+    public ChatMessageDto(String username, String message, LocalDateTime timestamp) {
+        this.username = username;
+        this.message = message;
+        this.timestamp = timestamp.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+    }
+
+}
