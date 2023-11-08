404b4989571ded54763f6d4b768271b4c3482ad7
heypop <hey@herenas.com>
2023-11-08 18:35:34 +0900

    Feat: 현재 접속중인 유저목록 구현 - 토큰 이슈 해결필요. (처음 접속시 JWT 오류발생)

--- src/main/java/com/starta/project/domain/liveQuiz/config/WebSocketConfig.java
+++ src/main/java/com/starta/project/domain/liveQuiz/config/WebSocketConfig.java
@@ -1,24 +1,20 @@
 package com.starta.project.domain.liveQuiz.config;
 
-import com.starta.project.domain.liveQuiz.handler.StompHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 @Configuration
 @EnableWebSocketMessageBroker
+@EnableWebSocket
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final StompHandler stompHandler;
+//    private final WebSocketHandler webSocketHandler;
 
     // 메시지 브로커가 /topic으로 시작하는 메시지가 메시지를 구독하는 클라이언트에게 라우팅 되도록 정의
     @Override
@@ -32,8 +28,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/api/ws").setAllowedOrigins("*");
     }
 
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(stompHandler);
-    }
 }
--- src/main/java/com/starta/project/domain/liveQuiz/service/LiveQuizService.java
+++ src/main/java/com/starta/project/domain/liveQuiz/service/LiveQuizService.java
@@ -3,6 +3,9 @@ package com.starta.project.domain.liveQuiz.service;
 
 import com.starta.project.domain.liveQuiz.dto.ChatMessageDto;
 import com.starta.project.domain.member.entity.Member;
+import com.starta.project.domain.member.entity.MemberDetail;
+import com.starta.project.domain.member.repository.MemberDetailRepository;
+import com.starta.project.domain.member.repository.MemberRepository;
 import com.starta.project.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,18 +21,23 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class LiveQuizService {
 
+    private final MemberDetailRepository memberDetailRepository;
+    private final MemberRepository memberRepository;
+
+    public String findNickName(String username) {
+        Member findMember = memberRepository.findByUsername(username).orElseThrow(() -> new IllegalArgumentException("회원 정보를 찾을 수 없습니다."));
+        return findMember.getMemberDetail().getNickname();
+    }
 
     public ChatMessageDto sendMessage(ChatMessageDto chatMessage) {
-        log.warn("chatMessage : {}", chatMessage);
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-//        Member member = userDetails.getMember();
-
-//        System.out.println(member.getId());
         if (chatMessage != null && chatMessage.getMessage() != null) {
             String escapedMessage = HtmlUtils.htmlEscape(chatMessage.getMessage());
-//            chatMessage = new ChatMessageDto(member.getId(), member.getUsername(), escapedMessage, LocalDateTime.now());
+            chatMessage = new ChatMessageDto(chatMessage.getNickName(), escapedMessage, LocalDateTime.now());
         }
         return chatMessage;
+    }
+
+    private MemberDetail findMember(String nickName) {
+        return memberDetailRepository.findByNickname(nickName).orElseThrow(() -> new IllegalArgumentException("회원 정보를 찾을 수 없습니다."));
     }
 }
