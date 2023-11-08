17fd2795faa697a66fdc62926ab39ba9ce758480
heypop <hey@herenas.com>
2023-11-08 14:42:48 +0900

    Feat: 인증객체를 정상적으로 받아오는지 확인 중

--- src/main/java/com/starta/project/domain/liveQuiz/controller/LiveQuizController.java
+++ src/main/java/com/starta/project/domain/liveQuiz/controller/LiveQuizController.java
@@ -2,14 +2,20 @@ package com.starta.project.domain.liveQuiz.controller;
 
 import com.starta.project.domain.liveQuiz.dto.ChatMessageDto;
 import com.starta.project.domain.liveQuiz.service.LiveQuizService;
+import com.starta.project.domain.member.entity.Member;
 import com.starta.project.global.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,12 +23,20 @@ public class LiveQuizController {
 
     private final LiveQuizService liveQuizService;
 
-    @MessageMapping("/liveChatRoom") //app/liveSendMassage
+    @MessageMapping("/liveChatRoom")
     @SendTo("/topic/liveChatRoom")
     public ChatMessageDto sendMessage(
-            @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails,
-            ChatMessageDto chatMessage) {
-        return liveQuizService.sendMessage(userDetails.getMember(),chatMessage);
+            ChatMessageDto chatMessage,
+            StompHeaderAccessor accessor) {
+
+        // StompHeaderAccessor를 통해 인증 정보에 접근
+        Principal userPrincipal = accessor.getUser();
+
+        System.out.println("principal : " + userPrincipal);
+        System.out.println("principal : " + userPrincipal.getName());
+
+
+        return liveQuizService.sendMessage(chatMessage);
     }
 
 }
--- src/main/java/com/starta/project/domain/liveQuiz/service/LiveQuizService.java
+++ src/main/java/com/starta/project/domain/liveQuiz/service/LiveQuizService.java
@@ -3,8 +3,11 @@ package com.starta.project.domain.liveQuiz.service;
 
 import com.starta.project.domain.liveQuiz.dto.ChatMessageDto;
 import com.starta.project.domain.member.entity.Member;
+import com.starta.project.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.HtmlUtils;
 
@@ -16,11 +19,16 @@ import java.time.LocalDateTime;
 public class LiveQuizService {
 
 
-    public ChatMessageDto sendMessage(Member member, ChatMessageDto chatMessage) {
+    public ChatMessageDto sendMessage(ChatMessageDto chatMessage) {
         log.warn("chatMessage : {}", chatMessage);
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+//        Member member = userDetails.getMember();
+
+//        System.out.println(member.getId());
         if (chatMessage != null && chatMessage.getMessage() != null) {
             String escapedMessage = HtmlUtils.htmlEscape(chatMessage.getMessage());
-            chatMessage = new ChatMessageDto(member.getId(), member.getUsername(), escapedMessage, LocalDateTime.now());
+//            chatMessage = new ChatMessageDto(member.getId(), member.getUsername(), escapedMessage, LocalDateTime.now());
         }
         return chatMessage;
     }
