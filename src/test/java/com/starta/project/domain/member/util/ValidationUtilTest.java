030190e83ff3fb2f0beb90ef21903c35a134e8b3
JH <jihyeon4956@gmail.com>
2023-11-09 07:44:19 +0900

    Test: username 중복여부 확인- 예외발생x

--- src/test/java/com/starta/project/domain/member/util/ValidationUtilTest.java
+++ src/test/java/com/starta/project/domain/member/util/ValidationUtilTest.java
@@ -2,8 +2,13 @@ package com.starta.project.domain.member.util;
 
 import com.starta.project.domain.member.entity.Member;
 import com.starta.project.domain.member.entity.MemberDetail;
+import com.starta.project.domain.member.entity.UserRoleEnum;
 import com.starta.project.domain.member.repository.MemberDetailRepository;
 import com.starta.project.domain.member.repository.MemberRepository;
+import com.starta.project.domain.quiz.entity.Comment;
+import com.starta.project.domain.quiz.entity.Quiz;
+import com.starta.project.domain.quiz.repository.CommentRepository;
+import com.starta.project.domain.quiz.repository.QuizRepository;
 import com.starta.project.global.messageDto.MsgResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -31,30 +36,52 @@ class ValidationUtilTest {
     @Mock
     private MemberDetailRepository memberDetailRepository;
 
+    @Mock
+    private QuizRepository quizRepository;
+
+    @Mock
+    private CommentRepository commentRepository;
+
     @InjectMocks
     private ValidationUtil validationUtil;
 
     private BindingResult bindingResult;
+    private Member member;
+    private MemberDetail memberDetail;
+    private Quiz quiz;
+    private Comment comment;
+
+
 
     @BeforeEach
     void setUp() {
-        bindingResult = new BeanPropertyBindingResult(null, "");
+        member = new Member("username", "password1!", UserRoleEnum.USER);
+        memberDetail = new MemberDetail("nick1");
+        quiz = new Quiz();
+        comment = new Comment();
+
+        bindingResult = new BeanPropertyBindingResult(member, "member");
     }
 
     @Test
-    @DisplayName("Username 중복체크")
-    void checkDuplicatedUsername_ThrowsExceptionIfUsernameExists() {
-        // Given
-        String username = "testUser";
-        when(memberRepository.findByUsername(username)).thenReturn(Optional.of(new Member()));
-
-        // When / Then
-        IllegalArgumentException exception = assertThrows(
-                IllegalArgumentException.class,
-                () -> validationUtil.checkDuplicatedUsername(username)
-        );
-        assertEquals("중복된 username 입니다.", exception.getMessage());
+    @DisplayName("1. 유효성 검사 확인 - 오류발생시 등록한 상태코드 반환 검증")
+    void signup_exception(){
+        // bindingResult에러 직접 발생
+        bindingResult.rejectValue("username", "error.username", "Username is required");
+        Optional<ResponseEntity<MsgResponse>> result = validationUtil.checkSignupValid(bindingResult);
+        assertTrue(result.isPresent());  // 오류 메세지가 있을 경우
+        assertEquals(400, result.get().getStatusCodeValue());  // 400에러 발생하는지 확인
     }
 
+    @Test
+    @DisplayName("2. Username 중복체크 - 중복에 맞게 값 반환")
+    void checkDuplicatedUsername() {
+        // Given
+        String username = "testuser";
+        when(memberRepository.findByUsername(username))
+                .thenReturn(Optional.empty());
+        // When / Then
+        assertDoesNotThrow(() -> validationUtil.checkDuplicatedUsername(username));
+    }
 
 }
