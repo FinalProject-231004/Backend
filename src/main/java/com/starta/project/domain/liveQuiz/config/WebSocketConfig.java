e01b4015ae72cfe29f890639c8ff9f1b971ba0d9
heypop <hey@herenas.com>
2023-11-08 14:42:31 +0900

    Feat: 인증객체 생성을 위한 핸들러 및 Config 변경

--- src/main/java/com/starta/project/domain/liveQuiz/config/WebSocketConfig.java
+++ src/main/java/com/starta/project/domain/liveQuiz/config/WebSocketConfig.java
@@ -1,14 +1,25 @@
 package com.starta.project.domain.liveQuiz.config;
 
+import com.starta.project.domain.liveQuiz.handler.StompHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
+
     // 메시지 브로커가 /topic으로 시작하는 메시지가 메시지를 구독하는 클라이언트에게 라우팅 되도록 정의
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -19,5 +30,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/api/ws").setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
