3f66552ece9d6e0d4acda24a1b0e247480df91d3
JH <jihyeon4956@gmail.com>
2023-11-11 05:04:24 +0900

    Refactor: ReportRepository 쿼리최적화

--- src/main/java/com/starta/project/domain/member/controller/ReportController.java
+++ src/main/java/com/starta/project/domain/member/controller/ReportController.java
@@ -4,6 +4,7 @@ import com.starta.project.domain.member.service.ReportService;
 import com.starta.project.global.messageDto.MsgResponse;
 import com.starta.project.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.Around;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
